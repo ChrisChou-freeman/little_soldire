36ac9658ab07edfa3434ac13ad0dd689997ef33a
ChrisChou-freeman <zhou.guanwei@foxmail.com>
2021-12-02 22:25:24 +0800

    update

--- engine/lib/__init__.py
+++ engine/lib/__init__.py
@@ -1,6 +1,7 @@
-from . import game_manager, menu, common_func
+from . import game_manager, menu, common_func, common_type
 
 GameManager = game_manager.GameManager
 Menu = menu.Menu
 
 com_fuc = common_func
+com_type = common_type
