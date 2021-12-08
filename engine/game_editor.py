bf72dd73e10800a9d786da8cd96cd9c3b0377fdc
ChrisChou-freeman <zhou.guanwei@foxmail.com>
2021-12-08 22:10:58 +0800

    update ui lib

--- engine/game_editor.py
+++ engine/game_editor.py
@@ -1,95 +1,101 @@
+from pygame import surface, event, Vector2, draw, image
 import pygame
-from pygame import surface, event, Vector2, draw
 
 from .lib import GameManager, com_fuc, com_type
+from .ui import Button
 from  . import settings
 
 class GameEditor(GameManager):
     def __init__(self, metadata: dict[str, str]) -> None:
         super().__init__(metadata)
-        self.background_lays: list[surface.Surface] = []
-        self.background_lays_pos: list[Vector2] = []
-        self.grid_line: list[com_type.Line] = []
-        self.layers_number = 2
+        self._background_lays: list[surface.Surface] = []
+        self._background_lays_pos: list[Vector2] = []
+        self._grid_line: list[com_type.Line] = []
+        self.tiles_button: Button
+        self._layers_repets = 2
         self._load_content()
-        self.scroll_speed = 2 * len(self.background_lays)
-        self.scroll_left = False
-        self.scroll_right = False
-        self.show_grid = False
+        self._scroll_speed = 2 * len(self._background_lays)
+        self._scroll_left = False
+        self._scroll_right = False
+        self._show_grid = False
 
     def _load_content(self) -> None:
-        self.background_lays = com_fuc.pygame_load_images_list(settings.GAME_PLAY_BACK_IMG_PATH)
-        last_backgrad_width = self.background_lays[-1].get_width()
-        self.background_lays_pos = [Vector2(r * last_backgrad_width, i*80) \
-                for r in range(self.layers_number)\
-                for i in range(len(self.background_lays))]
-        last_layer_width = self.background_lays[-1].get_width()
+        self._background_lays = com_fuc.pygame_load_images_list(settings.GAME_PLAY_BACK_IMG_PATH)
+        last_backgrad_width = self._background_lays[-1].get_width()
+        self._background_lays_pos = [Vector2(r * last_backgrad_width, i*80) \
+                for r in range(self._layers_repets)\
+                for i in range(len(self._background_lays))]
+        last_layer_width = self._background_lays[-1].get_width()
         for y_line in range(0, settings.SCREEN_HEIGHT, settings.TILE_SIZE[1]):
             line = com_type.Line(
                 Vector2(0, y_line),
-                Vector2(last_layer_width*self.layers_number, y_line)
+                Vector2(last_layer_width*self._layers_repets, y_line)
             )
-            self.grid_line.append(line)
-        for x_line in range(0, last_layer_width*self.layers_number, settings.TILE_SIZE[0]):
+            self._grid_line.append(line)
+        for x_line in range(0, last_layer_width*self._layers_repets, settings.TILE_SIZE[0]):
             line = com_type.Line(
                 Vector2(x_line, 0),
                 Vector2(x_line, settings.SCREEN_HEIGHT)
             )
-            self.grid_line.append(line)
+            self._grid_line.append(line)
+        buttn_img = image.load(settings.TILES_BTN_IMG_PATH)
+        self.tiles_button = Button(buttn_img, Vector2(20, 20))
 
     def _scroll_backgroud(self) -> None:
         scroll_speed = 0
-        if self.scroll_left and self.background_lays_pos[len(self.background_lays)-1].x >= 0:
+        if self._scroll_left and self._background_lays_pos[len(self._background_lays)-1].x >= 0:
             return
-        elif self.scroll_right and self.background_lays_pos[-1].x <= settings.SCREEN_WIDTH:
+        elif self._scroll_right and (self._background_lays_pos[-1].x + self._background_lays[-1].get_width()) <= settings.SCREEN_WIDTH:
             return
-        elif self.scroll_left or self.scroll_right:
-            scroll_speed = (self.scroll_speed * -1) if self.scroll_right else self.scroll_speed
-            lay_number = len(self.background_lays)
+        elif self._scroll_left or self._scroll_right:
+            scroll_speed = (self._scroll_speed * -1) if self._scroll_right else self._scroll_speed
+            lay_number = len(self._background_lays)
             last_layer_scl_speed = 0
-            for index, pos in enumerate(self.background_lays_pos):
+            for index, pos in enumerate(self._background_lays_pos):
                 current_lay = (index % lay_number) + 1
                 dif_lay_speed = current_lay / lay_number * scroll_speed
                 pos.x += dif_lay_speed
-                if index == len(self.background_lays_pos) - 1:
+                if index == len(self._background_lays_pos) - 1:
                     last_layer_scl_speed = dif_lay_speed
-            for line in self.grid_line:
+            for line in self._grid_line:
                 line.start_point.x += last_layer_scl_speed
                 line.eng_point.x += last_layer_scl_speed
 
     def handle_input(self, key_event: event.Event) -> None:
+        self.tiles_button.handle_input(key_event)
         if key_event.type == pygame.KEYDOWN:
             match key_event.key:
                 case pygame.K_a | pygame.K_LEFT:
-                    self.scroll_left = True
+                    self._scroll_left = True
                 case pygame.K_d | pygame.K_RIGHT:
-                    self.scroll_right = True
+                    self._scroll_right = True
                 case pygame.K_g:
-                    self.show_grid = False if self.show_grid else True
+                    self._show_grid = False if self._show_grid else True
                 case pygame.K_ESCAPE:
                     self.metadata['game_mode'] = 'GameStart'
         elif key_event.type == pygame.KEYUP:
             match key_event.key:
                 case pygame.K_a | pygame.K_LEFT:
-                    self.scroll_left = False
+                    self._scroll_left = False
                 case pygame.K_d | pygame.K_RIGHT:
-                    self.scroll_right = False
+                    self._scroll_right = False
 
     def update(self, _) -> None:
         self._scroll_backgroud()
 
     def _draw_grid(self, screen: surface.Surface) -> None:
-        if not self.show_grid:
+        if not self._show_grid:
             return
-        for line in self.grid_line:
+        for line in self._grid_line:
             draw.line(screen, settings.RGB_WHITE, line.start_point, line.eng_point)
 
     def draw(self, screen: surface.Surface) -> None:
-        for index,lay_pos in enumerate(self.background_lays_pos):
-            lay = self.background_lays[index%len(self.background_lays)]
+        for index,lay_pos in enumerate(self._background_lays_pos):
+            lay = self._background_lays[index%len(self._background_lays)]
             screen.blit(lay, lay_pos)
         self._draw_grid(screen)
+        self.tiles_button.draw(screen)
 
     def clear(self, screen: surface.Surface) -> None:
-        self.background_lays = []
+        self._background_lays = []
         screen.fill(settings.RGB_BLACK)
---
+++ engine/ui/menu_container.py
@@ -0,0 +1,6 @@
+from pygame import Rect, Vector2
+
+class MenuContainer:
+    def __init__(self, rec: Rect, position: Vector2):
+        self.rec = rec
+        self.position = position
