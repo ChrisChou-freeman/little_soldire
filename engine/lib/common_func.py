c7f1745d41f94c46ebdfb13078dff58c4907ee20
ChrisChou-freeman <ChrisChouFreeMan@hotmail.com>
2021-12-11 23:45:12 +0800

    update

--- engine/lib/common_func.py
+++ engine/lib/common_func.py
@@ -1,6 +1,9 @@
 import os
+import pickle
 
 from pygame import image, surface
+
+from .common_type import WorldDataStruct
 
 def listdir_clean(path: str) -> list[str]:
    files = os.listdir(path)
@@ -15,8 +18,13 @@ def pygame_load_iamges_with_name(path: str) -> dict[str, surface.Surface]:
     file_list = listdir_clean(path)
     return { file: image.load(os.path.join(path, file)) for file in file_list  }
 
-def load_world_data():
-    pass
+def load_world_data(world_data_path: str) -> WorldDataStruct:
+    with open(world_data_path, 'rb') as file_obj:
+        world_data_obj: WorldDataStruct = pickle.load(file_obj)
+        return world_data_obj
 
-def write_world_data():
-    pass
+def write_world_data(
+        world_data_path: str,
+        world_data_obj: WorldDataStruct) -> None:
+    with open(world_data_path, 'wb') as  file_obj:
+        pickle.dump(world_data_obj, file_obj)
