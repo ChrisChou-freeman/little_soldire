bb4eb1d257dc5aea87744a4c3b905b50e387571a
ChrisChou-freeman <zhou.guanwei@foxmail.com>
2021-12-21 16:58:09 +0800

    update

--- engine/game_play.py
+++ engine/game_play.py
@@ -1,11 +1,19 @@
-from pygame import surface, event
+from pygame import surface, event, Vector2
 
-from .lib import GameManager
+from .lib import GameManager, com_fuc
+from . import settings
 
 class GamePlay(GameManager):
 
     def __init__(self, metadata: dict[str, str]) -> None:
         super().__init__(metadata)
+        self._background_lays = com_fuc.pygame_load_images_list(settings.GAME_PLAY_BACK_IMG_PATH)
+        self._background_lays_pos: list[Vector2] = []
+        self._tiles_images = com_fuc.pygame_load_iamges_with_name(settings.TILES_IMG_PATH)
+        self._item_images = com_fuc.pygame_load_iamges_with_name(settings.ITEMS_IMG_PATH)
+        self._sprite_images = com_fuc.pygame_load_iamges_with_name(settings.SPRITE_IMG_PATH)
+        self._layers_repets = 2
+        self._current_level = 0
 
     def handle_input(self, key_event: event.Event) -> None:
         pass
