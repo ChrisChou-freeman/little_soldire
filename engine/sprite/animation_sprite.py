476ba638dd4acc4bc38ac2ef3d705a8eb3be6dac
ChrisChou-freeman <ChrisChouFreeMan@hotmail.com>
2022-01-01 17:24:03 +0800

    add run action

--- engine/sprite/animation_sprite.py
+++ engine/sprite/animation_sprite.py
@@ -4,7 +4,9 @@ class AnimationSprite(sprite.Sprite):
     def __init__(self,
                  image_sheet: surface.Surface,
                  position: Vector2,
-                 fram_with: int, loop: bool=True) -> None:
+                 fram_with: int,
+                 loop: bool=True,
+                 flip:bool=False) -> None:
         super().__init__()
         self._current_fram = 0
         self._loop = loop
@@ -15,6 +17,7 @@ class AnimationSprite(sprite.Sprite):
         self._fram_with = fram_with
         self._fram_number = self._image_sheet.get_width() / fram_with
         self.rotate_value = 0.0
+        self.flip = flip
         self.image = self._get_curren_fram()
         self.rect = self.image.get_rect()
         self.rect = self.rect.move(position)
@@ -28,7 +31,10 @@ class AnimationSprite(sprite.Sprite):
         )
 
     def _get_curren_fram(self) -> surface.Surface:
-        return transform.rotate(self._image_sheet.subsurface(self._get_curren_fram_area()), self.rotate_value)
+        image = transform.rotate(self._image_sheet.subsurface(self._get_curren_fram_area()), self.rotate_value)
+        if self.flip:
+            image = transform.flip(image, True, False)
+        return image
 
     def play(self) -> None:
         self._counter += 1
--- engine/sprite/role_sprite.py
+++ engine/sprite/role_sprite.py
@@ -1,4 +1,4 @@
-from pygame import Vector2, image
+from pygame import Vector2, image, transform
 
 from .animation_sprite import AnimationSprite
 from ..lib import GameDataStruct
@@ -12,14 +12,14 @@ class RoleSprite(AnimationSprite):
         self.position = position
         self._set_current_action()
 
-    def _set_current_action(self) -> None:
+    def _set_current_action(self, flip=False) -> None:
         action_info = self._sprite_sheet_info.get(self._action, None)
         if action_info is None:
             return
         image_sheet = image.load(action_info['image_sheet'])
         fram_with = int(action_info['fram_with'])
         loop = True if action_info['loop'] == '1' else False
-        super().__init__(image_sheet, self.position, fram_with, loop)
+        super().__init__(image_sheet, self.position, fram_with, loop, flip)
 
 
 class PlayerSprite(RoleSprite):
@@ -33,11 +33,17 @@ class PlayerSprite(RoleSprite):
     def update(self, *_, **kwargs) -> None:
         vec: Vector2 = kwargs['vec']
         action: str = kwargs['action']
+        if action.startswith('run_'):
+            if action == 'run_left':
+                self.flip = True
+            elif action == 'run_right':
+                self.flip = False
+            action = 'run'
         if self.rect is not None:
             vec.x, vec.y = self.world_data_obj.collections_detect(self.rect, int(vec.x), int(vec.y))
-            print(vec)
             self.rect = self.rect.move(vec)
+            self.position.x, self.position.y = self.rect.x, self.rect.y
         if action != self._action:
             self._action = action
-            self._set_current_action()
+            self._set_current_action(self.flip)
         self.play()
