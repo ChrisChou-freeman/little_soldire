5582a5509aea4c6b8074963c4d718dbee583a715
ChrisChou-freeman <ChrisChouFreeMan@hotmail.com>
2022-01-03 15:30:31 +0800

    add scroll function

--- engine/lib/common_type.py
+++ engine/lib/common_type.py
@@ -2,7 +2,6 @@ from typing import NamedTuple
 
 from pygame import Vector2
 
-
 class Line(NamedTuple):
     start_point: Vector2
     eng_point: Vector2
--- engine/sprite/item_sprite.py
+++ engine/sprite/item_sprite.py
@@ -1,8 +1,21 @@
 from pygame import sprite, Vector2, surface
 
+from ..lib import GameMetaData
+
 class ItemSprite(sprite.Sprite):
-    def __init__(self, image: surface.Surface, position: Vector2) -> None:
+    def __init__(self,
+                 image: surface.Surface,
+                 position: Vector2,
+                 metadata: GameMetaData) -> None:
         super().__init__()
         self.image = image
         self.rect = image.get_rect()
         self.rect = self.rect.move(position)
+        self.metadata = metadata
+
+    def update(self, *_, **__) -> None:
+        if self.rect is None:
+            return
+        self.rect.x += self.metadata.scroll_index
+        if self.rect.right < 0:
+            self.kill()
