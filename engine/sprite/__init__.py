b9fb85dfe26c11208e1e9939d6c3bd276cafae7e
ChrisChou-freeman <ChrisFreeMan@hotmail.com>
2022-01-03 15:51:44 +0800

    update

--- engine/sprite/__init__.py
+++ engine/sprite/__init__.py
@@ -4,6 +4,7 @@ AnimationSprite =  animation_sprite.AnimationSprite
 CloudSprite = cloud_sprite.CloudSprite
 RoleSprite = role_sprite.RoleSprite
 PlayerSprite = role_sprite.PlayerSprite
+EnemySprite = role_sprite.EnemySprite
 TileSprite = tile_sprite.TileSprite
 ItemSprite = item_sprite.ItemSprite
 
